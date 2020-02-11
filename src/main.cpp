d5ce9289fa499935d4e19bb458c8ffbb9bcd5f26
ruibo <david.yanrb@gmail.com>
2020-02-10 20:31:16 -0500

    * Added input checking for start and end coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -60,8 +60,38 @@ int main(int argc, const char **argv)
     float end_x;
     float end_y;
 
-    std::cin >> start_x >> start_y >> end_x >> end_y;
-
+    std::cout << "Enter a start_x from 0 to 100: ";
+    std::cin >> start_x;
+    while(!(std::cin) || start_x < 0 || start_x > 100){
+        std::cout << "Invalid entry. Enter a start_x from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> start_x;
+    }
+    std::cout << "Enter a start_y from 0 to 100: ";
+    std::cin >> start_y;
+    while(!(std::cin) || start_y < 0 || start_y > 100){
+        std::cout << "Invalid entry. Enter a start_y from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> start_y;
+    }
+    std::cout << "Enter a end_x from 0 to 100: ";
+    std::cin >> end_x;
+    while(!(std::cin) || end_x < 0 || end_x > 100){
+        std::cout << "Invalid entry. Enter a end_x from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> end_x;
+    }
+    std::cout << "Enter a end_y from 0 to 100: ";
+    std::cin >> end_y;
+    while(!(std::cin) || end_y < 0 || end_y > 100){
+        std::cout << "Invalid entry. Enter a end_y from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> end_y;
+    }
     // Build Model.
     RouteModel model{osm_data};
 
